d98d45b3c14ff22a5f74de3259e62ad7c0894144
TheLeerName <lesha.nechaev.05@mail.ru>
2021-12-06 00:28:17 +0700

    o

--- stages/chara/chara.lua
+++ stages/chara/chara.lua
@@ -2,7 +2,7 @@ function onCreate()
 	makeLuaSprite('bg', 'stages/chara/bg', -925, -100);
 	addLuaSprite('bg', false);
 
-	scaleObject('bg', 1.25, 1);
+	scaleObject('bg', 1.35, 1);
 
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+	close(true);
 end
